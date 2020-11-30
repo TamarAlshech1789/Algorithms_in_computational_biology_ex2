92e023cd19b4f44f40de8ab73c925c6af9c14e49
Tamar Alshech <tamaralshech@gmail.com>
2020-11-30 08:17:04 +0200

    first commit, only forward

---
+++ motif_find.py
@@ -0,0 +1,82 @@
+import argparse
+import pandas as pd
+import numpy as np
+
+def build_tau_forward(p, k, num_states, index_b1, index_b2, index_b_end):
+
+    tau = [[0 for j in range(num_states)] for i in range(num_states)]
+    tau[index_b1][index_b1] = 1 - p
+    tau[index_b1][0] = p
+    tau[index_b2][index_b2] = 1 - p
+    tau[index_b2][index_b_end] = p
+    for i in range(k - 1):
+        tau[i][i + 1] = 1
+    tau[k - 1][index_b2] = 1
+    return tau
+
+def forward(seq,initial_emission, p, q):
+
+    emission_table = pd.read_csv(initial_emission, sep='\t')
+    k = len(emission_table['A'])
+    num_rows = k + 3
+    emission_table['$'] = [0 for i in range(k)]
+    emission_table_bottom = [[0 for j in range(5)] for i in range(num_rows - k)]
+    for j in range(4):
+        for i in range(2):
+            emission_table_bottom[i][j] = 0.25
+    emission_table_bottom[2][4] = 1
+    emission_table_bottom = pd.DataFrame(emission_table_bottom)
+    emission_table_bottom.columns = emission_table.columns
+    emission_table_bottom.index = [i + k for i in range (num_rows - k)]
+
+    print(emission_table)
+    emission_table = emission_table.append(emission_table_bottom)
+    print(emission_table)
+
+    #k = len(emission_table['A'])
+    #num_rows = k + 3
+    num_cols = len(seq) + 1         #todo : has a column to B_end
+    index_b1 = num_rows - 3
+    index_b2 = num_rows - 2
+    index_b_end = num_rows -1
+    forward_table = [[0 for j in range(num_cols)] for i in range(num_rows)]
+    tau = build_tau_forward(p, k, num_rows, index_b1, index_b2, index_b_end)
+
+    print(forward_table)
+    print(tau)
+
+    forward_table[index_b1][0] = q * 0.25
+    forward_table[index_b2][0] = (1 - q) * 0.25
+    # for j in range(1, num_cols):   #todo : without te last column B_end
+    #     for i in range(num_rows):
+    #         if i == num_rows - 2:
+    #             tau = 1-p
+    #         elif i == num_rows - 1:
+    #             if j ==
+    #
+    #         forward_table[i][j] += forward_table[i][j-1] *
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--alg', help='Algorithm (e.g. viterbi)', required=True)
+    parser.add_argument('seq', help='A sequence over the alphabet [A,C,G,T] (e.g. ACTGGACTACGTCATGCA)')
+    parser.add_argument('initial_emission', help='Path to emission table (e.g. initial_emission.tsv)')
+    parser.add_argument('p', help='transition probability p (e.g. 0.01)', type=float)
+    parser.add_argument('q', help='transition probability q (e.g. 0.5)', type=float)
+    args = parser.parse_args()
+
+    if args.alg == 'viterbi':
+        raise NotImplementedError
+
+    elif args.alg == 'forward':
+        forward(args.seq, args.initial_emission, args.p, args.q)
+
+    elif args.alg == 'backward':
+        raise NotImplementedError
+
+    elif args.alg == 'posterior':
+        raise NotImplementedError
+
+
+if __name__ == '__main__':
+    main()
